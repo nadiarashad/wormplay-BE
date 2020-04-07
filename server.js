5aad69eb6049fdebfdc89359d61adbdd363eff25
nadiarashad <nadiarashid@live.com>
2020-04-07 11:13:55 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  console.log("New client connected again");
+
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+const getApiAndEmit = async (socket) => {
+  try {
+    const res = await axios.get(
+      "http://api.weatherapi.com/v1/current.json?key=0923d95ae0ed4df3a7190232200604&q=Manchester"
+    );
+    socket.emit("FromAPI", res.data); // Emitting a new message. It will be consumed by the client
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
