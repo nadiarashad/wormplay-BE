882113b3bd5c41772d9bebb94bfeca6cd32ec0f9
nadiarashad <nadiarashid@live.com>
2020-04-08 13:04:58 +0100

    pushing to heroku

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ server.listen(port, () => console.log(`Listening on port ${port}`));
 
 io.on("connection", function (socket) {
   console.log("New client connected");
+  // console.log(socket, "socket");
 
   io.emit("news", { hello: stringToBroadcast });
 
@@ -29,27 +30,37 @@ io.on("connection", function (socket) {
     socket.broadcast.emit("server update", { hello: stringToBroadcast });
   });
 
-  socket.on("login", function (data) {
-    const newPlayer = {};
-    newPlayer.id = socket.id; // also data.id the same
-    newPlayer.handle = "";
-
-    if (playersInGame[0] === "") {
-      playersInGame[0] = newPlayer;
-      console.log(playersInGame, "added player1");
-      io.to(socket.id).emit("socket.id", {
-        p1: true,
-      });
-    } else if (playersInGame[1] === "") {
-      playersInGame[1] = newPlayer;
-      console.log(playersInGame, "added player2");
-      io.to(playersInGame[0]).emit("gameEntry", {
-        playersArr: playersInGame,
-      });
-    } else {
-      io.to(socket.id).emit("sendMsg", {});
-    }
+  socket.on("p1ArrayUpdate", function (data) {
+    console.log("updating p1array");
+    arrayToBroadcast = data.p1Chars;
+    socket.broadcast.emit("updatedP1Chars", { p1Chars: arrayToBroadcast });
   });
+
+  socket.on("p2ArrayUpdate", function (data) {
+    console.log("updating p2array");
+    arrayToBroadcast = data.p2Chars;
+    socket.broadcast.emit("updatedP2Chars", { p2Chars: arrayToBroadcast });
+  });
+
+  const newPlayer = {};
+  newPlayer.id = socket.id; // also data.id the same
+  newPlayer.handle = "";
+
+  if (playersInGame[0] === "") {
+    playersInGame[0] = newPlayer;
+    console.log(playersInGame, "added player1");
+    io.to(socket.id).emit("player1", {
+      p1: true,
+    });
+  } else if (playersInGame[1] === "") {
+    playersInGame[1] = newPlayer;
+    console.log(playersInGame, "added player2");
+    io.to(socket.id).emit("player2", {
+      p2: true,
+    });
+  } else {
+    io.to(socket.id).emit("sendMsg", {});
+  }
 
   socket.on("disconnect", () => {
     if (socket.id === playersInGame[0].id) {
