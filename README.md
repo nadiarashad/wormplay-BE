95c14716ede5c7c7edeb19ed7df53e8dda68aa99
C <chrismatusemail@gmail.com>
2020-04-10 10:11:51 +0100

    Tidied som mock game fxns, and also added nodemon shortcut.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# womrnplay_backendserver
+I've added a nodemon shortcut, so instead of typing `node server.js` every time, and having to kill and restart whenever you make changes, you can instead just type **npm run mon** and it will update for live changes.
+
+The _mon_ stands for _nodemon_.
--- server.js
+++ server.js
@@ -14,36 +14,40 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 // Variables for storing clients in game and in lobby
-let stringToBroadcast = "this";
+
 const playersInGame = {
   p1: { username: null, id: null },
   p2: { username: null, id: null },
 };
-
+const labelArray = ["p1", "p2"];
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
 io.on("connection", function (socket) {
-  console.log("New client connected");
-
-  io.emit("news", { hello: stringToBroadcast });
+  console.log(`Connection of ${socket.id}`);
 
   socket.on("login", function (data) {
+    console.log(`${data.username} is loggin in. Their socket is ${socket.id}`);
     const newPlayer = {};
     newPlayer.id = socket.id;
     newPlayer.username = data.username;
 
+    function enterGame(player) {
+      playersInGame[player] = newPlayer;
+      io.to(socket.id).emit("loginConf", {
+        youCanEnter: true,
+        playersDetails: playersInGame,
+      });
+      socket.broadcast.emit("a player entered the game", {
+        playersDetails: playersInGame,
+        enteringPlayerID: socket.id,
+        enteringPlayerUsername: playersInGame[player].username,
+      });
+    }
+
     if (!playersInGame.p1.id) {
-      playersInGame.p1 = newPlayer;
-      io.to(socket.id).emit("loginConf", {
-        youCanEnter: true,
-        playersDetails: playersInGame,
-      });
+      enterGame("p1");
     } else if (!playersInGame.p2.id) {
-      playersInGame.p2 = newPlayer;
-      io.to(socket.id).emit("loginConf", {
-        youCanEnter: true,
-        playersDetails: playersInGame,
-      });
+      enterGame("p2");
     } else {
       io.to(socket.id).emit("loginConf", {
         youCanEnter: false,
@@ -51,31 +55,31 @@ io.on("connection", function (socket) {
     }
   });
 
-  socket.on("p1ArrayUpdate", function (data) {
-    console.log("updating p1array");
-    arrayToBroadcast = data.p1Chars;
-    socket.broadcast.emit("updatedP1Chars", { p1Chars: arrayToBroadcast });
-  });
-
-  socket.on("p2ArrayUpdate", function (data) {
-    console.log("updating p2array");
-    arrayToBroadcast = data.p2Chars;
-    socket.broadcast.emit("updatedP2Chars", { p2Chars: arrayToBroadcast });
-  });
-
   socket.on("disconnect", () => {
-    if (socket.id === playersInGame.p1.id) {
-      io.emit("player left", {
-        leavingPlayerID: playersInGame.p1.id,
-        leavingPlayerUsername: playersInGame.p1.username,
-      });
-      playersInGame.p1 = { username: null, id: null };
-    } else if (socket.id === playersInGame.p2.id) {
-      io.emit("player left", {
-        leavingPlayerID: playersInGame.p2.id,
-        leavingPlayerUsername: playersInGame.p2.username,
-      });
-      playersInGame.p2 = { username: null, id: null };
-    }
+    console.log(`Disconnection of ${socket.id}`);
+
+    labelArray.forEach((player) => {
+      if (socket.id === playersInGame[player].id) {
+        const leavingPlayerUsername = playersInGame[player].username;
+        playersInGame[player] = { username: null, id: null };
+        socket.broadcast.emit("a player left the game", {
+          playersDetails: playersInGame,
+          leavingPlayerID: socket.id,
+          leavingPlayerUsername,
+        });
+      }
+    });
   });
+  //THIS WAS FROM MOCK GAME.
+  // socket.on("p1ArrayUpdate", function (data) {
+  //   console.log("updating p1array");
+  //   arrayToBroadcast = data.p1Chars;
+  //   socket.broadcast.emit("updatedP1Chars", { p1Chars: arrayToBroadcast });
+  // });
+
+  // socket.on("p2ArrayUpdate", function (data) {
+  //   console.log("updating p2array");
+  //   arrayToBroadcast = data.p2Chars;
+  //   socket.broadcast.emit("updatedP2Chars", { p2Chars: arrayToBroadcast });
+  // });
 });
