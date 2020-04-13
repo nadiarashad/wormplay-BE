a002ac43949ba835e7c8ad65ac93db5867023fa2
Patrick Mackridge <pat.mack@talktalk.net>
2020-04-13 14:39:40 +0100

    Server takes submitted word and validates it through axios request to oxford-dictionary-api. Result sent to players

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const http = require("http");
 const socketIo = require("socket.io");
+const { validateWord } = require("./utils/utils");
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
@@ -57,7 +58,39 @@ io.on("connection", function (socket) {
 
   socket.on("worm word submitted", function (wormWord) {
     console.log("Worm Word Received:", wormWord);
-    // Send to dictionary API - if not real word NO POINTS if real word POINTS = wormWord.length;
+    validateWord(wormWord)
+      .then((res) => {
+        io.to(socket.id).emit("word checked", {
+          word: wormWord,
+          isValid: true,
+          points: wormWord.length,
+        });
+        socket.broadcast.emit("opponent score", {
+          word: wormWord,
+          isValid: true,
+          opponentPoints: wormWord.length,
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          io.to(socket.id).emit("word checked", {
+            word: wormWord,
+            isValid: false,
+            points: 0,
+          });
+          socket.broadcast.emit("opponent score", {
+            word: wormWord,
+            isValid: false,
+            opponentPoints: 0,
+          });
+        } else {
+          io.to(socket.id).emit("api error", {
+            status: error.response.status,
+            message: error.response.statusText,
+          });
+        }
+      });
+    // Some p1 points vs p2 points to show who wins?? (probably separate function/event)
   });
 
   socket.on("disconnect", () => {
