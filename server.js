faac0ee68d47209e8cdcf4e784f9671f6e5c2cd8
Brork <jamessamueljohnson@gmail.com>
2020-04-16 11:06:46 +0100

    added socket for transfer of letters between players

--- server.js
+++ server.js
@@ -56,6 +56,14 @@ io.on("connection", function (socket) {
     }
   });
 
+  socket.on("playerChangesLetter", function (data) {
+    const { index, character } = data;
+    socket.broadcast.emit("opponentUpdates", {
+      character: character,
+      index: index,
+    });
+  });
+
   socket.on("worm word submitted", function (wormWord) {
     console.log("Worm Word Received:", wormWord);
     validateWord(wormWord)
