e42801f78a52f223a431c35cff5a52605d27e7c5
C <chrismatusemail@gmail.com>
2020-04-16 12:41:08 +0100

    roomupdate-continued

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ response: "This is the wormplay server." }).status(200);
+  res.send({ response: "Thurs pm this is the wormplay server." }).status(200);
 });
 
 module.exports = router;
